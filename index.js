7b4ff398c76bd5aaf685b182c9dc8a7ab580daab
zhxlp <1573635222@qq.com>
2022-06-09 13:18:19 +0800

    react-native-share-menu android

--- index.js
+++ index.js
@@ -3,8 +3,12 @@
  */
 
 import 'react-native-gesture-handler';
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Platform} from 'react-native';
 import App from './src/App';
+import Share from './src/Share';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+if (Platform.OS === 'ios') {
+  AppRegistry.registerComponent('ShareMenuModuleComponent', () => Share);
+}
--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,50 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {RootStackScreen} from '@routes';
 import {navigationRef} from '@routes/navigationUtils';
-import React from 'react';
-
+import React, {useEffect} from 'react';
+import ShareMenu, {ShareData} from 'react-native-share-menu';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import RNFS from 'react-native-fs';
 
+const handleShare = (share?: ShareData) => {
+  console.log(share);
+  if (!share || !share.data) {
+    return;
+  }
+  if (
+    share.mimeType === 'text/plain' &&
+    typeof share.data === 'string' &&
+    !share.data.startsWith('content://')
+  ) {
+    console.log('文本');
+    return;
+  }
+  let data: string[] = [];
+  if (typeof share.data === 'string') {
+    data = [share.data];
+  } else {
+    data = share.data;
+  }
+  try {
+    data.forEach(async uri => {
+      const res = await RNFS.stat(uri);
+      console.log(res);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
 const App = () => {
+  useEffect(() => {
+    ShareMenu.getInitialShare(handleShare);
+  }, []);
+  useEffect(() => {
+    const listener = ShareMenu.addNewShareListener(handleShare);
+
+    return () => {
+      listener.remove();
+    };
+  }, []);
   return (
     <SafeAreaProvider>
       <NavigationContainer ref={navigationRef}>
